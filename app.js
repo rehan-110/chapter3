da17ce7cc16c54a0cd5b76734f649a629f0b8468
rehan-110 <ar4084480@gmail.com>
2024-09-06 16:10:03 +0500

    chapter 3 files

---
+++ app.js
@@ -0,0 +1,16 @@
+//chapter 3
+
+var age = "15", visitTime = "“You have visited this site 10 times", birthYear = 2000;
+alert("I am " + age + " YearsOld");
+alert(visitTime);
+document.write("My Birth Year Is " + birthYear);
+document.write(" Data Type Of My Declared variable is Number");
+
+
+
+
+//chapter 3-4.
+
+var visitorName = "John Doe" , ProductTitle = "T-Shirts" , Qty = "14", storeName = "XYZ"
+
+document.write( visitorName + " Ordered " + Qty + ProductTitle + " On " + storeName + " Store ");
